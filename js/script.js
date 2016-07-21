36f7f5d2c216c5c62c93f1a2a9ccaa192fbf65ed
Bjorkbat <bjorkbat@gmail.com>
2016-07-21 10:31:11 -0600

    added images and first block

--- js/script.js
+++ js/script.js
@@ -7,9 +7,25 @@ var game = new Phaser.Game(
 );
 
 function preload() {
+
+  // Load our blocks
+  game.load.image('i-block', '/img/iblock.png');
+  game.load.image('o-block', '/img/oblock.png');
+  game.load.image('t-block', '/img/tblock.png');
+  game.load.image('l-block', '/img/lblock.png');
+  game.load.image('j-block', '/img/jblock.png');
+  game.load.image('s-block', '/img/sblock.png');
+  game.load.image('z-block', '/img/zblock.png');
 }
 
 function create() {
+
+  // Add a random tetris sprite to the game
+  block = game.add.sprite(
+    game.world.centerX, // x position
+    0,                  // y position
+    'o-block'           // keys
+  );
 }
 
 function update() {
